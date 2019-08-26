f15a73f12df1ec64b2e847f8660908063f951f69
ChuckBTaylor <WLScripps@gmail.com>
2019-08-26 11:03:48 +0300

    Updating Componenets with models

--- src/app/components/Cards/their-card.component.ts
+++ src/app/components/Cards/their-card.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { ThierCard } from 'src/app/models/their-card.model';
 
 @Component({
     selector: 'their-card',
     templateUrl: './their-card.component.html'
 })
-export class TheirCardComponent {
-    cardColor: string = "";
-    cardNumber: string = "";
+export class TheirCardComponent extends ThierCard {
+    
 }
