bdbc65d693a8d52f100bd0da94f7e7634b83b69f
ChuckBTaylor <WLScripps@gmail.com>
2019-11-24 13:16:04 +0300

    Updating table component

--- src/app/components/Table/table.component.ts
+++ src/app/components/Table/table.component.ts
@@ -1,9 +1,22 @@
 import { Component } from "@angular/core";
+import { PlayAreaComponent } from './play-area.component';
+import { GarbageAreaComponent } from './garbage-area.component';
+import { GameInfoComponent } from './game-info.component';
+import { MyHandComponent } from '../Hands/my-hand.component';
+import { TheirHandComponent } from '../Hands/their-hand.component';
+import { ICard } from 'src/app/models/Card/card.model';
 
 @Component({
     selector: 'hanabi-table',
     templateUrl: './table.component.html'
 })
 export class TableComponent {
+
+    playArea: PlayAreaComponent;
+    garbageAre: GarbageAreaComponent;
+    gameInfo: GameInfoComponent;
+    myHand: MyHandComponent;
+    theirHands: TheirHandComponent[];
+    deck: ICard[];
 
 }
